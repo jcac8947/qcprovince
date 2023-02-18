1836faaf5629dcfbbf19a8b5a55c84747b234401
abneremmanuel123 <abneremmanuel123@gmail.com>
2023-02-19 00:40:53 +0800

    adding Details

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Cards from './components/Cards';
 import Navbar from './components/Navbar';
  
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" exact />
             </Routes>
+            <Cards/>
         </Router>
         </>
     );
