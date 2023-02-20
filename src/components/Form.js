0d777983329a93a8e50010b7f27e4a276dd554ff
abneremmanuel123 <abneremmanuel123@gmail.com>
2023-02-20 20:17:02 +0800

    fixing validation form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,24 +3,39 @@ import './pages/SIgnUp.css'
 import "./Form.css";
 
 function Form() {
-  (function () {
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  // (function () {
+  
+  //   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  //   var forms = document.querySelectorAll('.needs-validation')
+    
+  //   // Loop over them and prevent submission
+  //   Array.prototype.slice.call(forms)
+  //     .forEach(function (form) {console.log('Hello World', form)
+  //       form.addEventListener('submit', function (event) {
+          
+  //         if (!form.checkValidity()) {
+  //           event.preventDefault()
+  //           event.stopPropagation()
+  //         }
+  
+  //         form.classList.add('was-validated')
+  //       }, false)
+  //     })
+  // })()
+
+  function handleSubmit(e){
+    e.preventDefault();
     var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
+    
+     Array.prototype.slice.call(forms)
       .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
           if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
+            e.preventDefault();
           }
-  
           form.classList.add('was-validated')
-        }, false)
       })
-  })()
+  }
 
   return (
     <div className="bg">
@@ -74,7 +89,7 @@ function Form() {
             </div>
           </div>
           <p></p>
-          <button className="btn btn-primary" type="submit">Submit</button>
+          <button className="btn btn-primary" type="submit" onClick={(e)=> handleSubmit(e)}>Submit</button>
         </div>
       </form>
     </div>
