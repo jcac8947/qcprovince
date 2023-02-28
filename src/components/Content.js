2f090f54fbd3cd603186822762af7bfe9ef8070b
timsayala <timsayala1218@gmail.com>
2023-02-28 12:37:51 +0800

    fix the code for the Content.js

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,57 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './ContentSlider.css';
-import { ContentSlider } from './ContentSlider';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import { ContentSlider } from './ContentSlider';
+import balesin from '../Images/balesin.jpg';
+import kehalani from '../Images/Kehalani.jpg';
+import nilandingan from '../Images/Nilandingan.jpg';
+import queen from '../Images/queen.jpg';
+import batis from '../Images/batis.jpeg';
 
+const images = [balesin, kehalani, nilandingan, queen, batis];
 
-const Content = ({ slides }) => {
+const Content = () => {
   const [current, setCurrent] = useState(0);
-  const [description, setDescription] = useState('');
-  const lenght = slides.length;
 
-  const [imageUrl, setImageUrl] = useState('')
+  useEffect(() => {
+    // Load all the images in advance
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+    });
+  }, []);
 
   const nextSlide = () => {
-    setCurrent(current === lenght - 1 ? 0 : current + 1);
+    setCurrent(current === ContentSlider.length - 1 ? 0 : current + 1);
   };
 
   const prevSlide = () => {
-    setCurrent(current === 0 ? lenght - 1 : current - 1);
+    setCurrent(current === 0 ? ContentSlider.length - 1 : current - 1);
   };
 
-  if (!Array.isArray(slides) || slides.lenght <= 0) {
-    return null;
-  }
-
-  const loadImage = async (path) => {
-    const image = await import(`../Images/${path}`);
-    //console.log(image)
-    setImageUrl(image.default);
-  }
-
-
-
   return (
-    
-      <section className='slider'>
-        <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
-        <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
-        {ContentSlider.map((slide, index) => {
-          loadImage(slide.image)
-          return (
-            <div className={index === current ? 'slide active' : 'slide'} key={index}>
-              <h3 className='image-title'>{slide.title}</h3>
-              {index === current && (
-                <img src={imageUrl} alt='travel image' className='image' onMouseEnter={() => setDescription(slide.description)} onMouseLeave={() => setDescription('')} />
-              )}
-              {index === current && (
-                <div className='description'>{description}</div>
-              )}
-            </div>
-          );
-        })}
-      </section>
-    );
+    <section className='slider'>
+      <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+      <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+      {ContentSlider.map((slide, index) => {
+        return (
+          <div className={index === current ? 'slide active' : 'slide'} key={index}>
+            <h3 className='image-title'>{slide.title}</h3>
+            {index === current && (
+              <img
+                src={images[index]}
+                alt='travel image'
+                className='image'
+              />
+            )}
+            {index === current && <div className='description'>{slide.description}</div>}
+          </div>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Content;
