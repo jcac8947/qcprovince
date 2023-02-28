8c51c56c7203dac702df3473c5e6c0086dce0220
timsayala <timsayala1218@gmail.com>
2023-02-28 16:29:30 +0800

    fix the slides in the destination

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,25 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Slider.css';
-import { SliderData } from './SliderData';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import { SliderData } from './SliderData';
+import salibungot from '../Images/salibungot_beach.JPG';
+import borawan from '../Images/borawan_island.JPG';
+import sunshine from '../Images/sunshine_farm.JPG';
+import banahaw from '../Images/mount_banahaw.JPG';
+import escudero from '../Images/villaEscudero.JPG';
+import balagbag from '../Images/balabag_falls.JPG';
+import mirador from '../Images/mount_mirador.jpg';
+import kanaway from '../Images/kanaway_beach.JPG';
+import lampas from '../Images/kwebang_lampas.JPG';
+import dampalitan from '../Images/dampalitan_island.JPG';
 
+const images = [salibungot,borawan,sunshine,banahaw,escudero,balagbag,mirador,kanaway,lampas,dampalitan];
 
-const ImageSlider = ({ slides }) => {
+const ImageSlider = () => {
   const [current, setCurrent] = useState(0);
-  const [description, setDescription] = useState('');
-  const slider = slides.length;
+  
 
+  useEffect(() => {
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+    });
+  }, [] );
+  
   const nextSlide = () => {
-    setCurrent(current === slider - 1 ? 0 : current + 1);
+    setCurrent(current === SliderData.length - 1 ? 0 : current + 1);
   };
 
   const prevSlide = () => {
-    setCurrent(current === 0 ? slider - 1 : current - 1);
+    setCurrent(current === 0 ? SliderData.length - 1 : current - 1);
   };
 
-  if (!Array.isArray(slides) || slides.slider <= 0) {
-    return null;
-  }
 
   return (
     
@@ -31,12 +45,13 @@ const ImageSlider = ({ slides }) => {
             <div className={index === current ? 'slide active' : 'slide'} key={index}>
               <h3 className='image-title'>{slide.title}</h3>
               {index === current && (
-                <img src={slide.image} alt='travel image' className='image' onMouseEnter={() => setDescription(slide.description)} onMouseLeave={() => setDescription('')} />
+                <img src={images[index]}
+                 alt='travel image' 
+                 className='image'
+                  />
               )}
-              {index === current && (
-                <div className='description'>{description}</div>
-              )}
-            </div>
+             {index === current && <div className='description'>{slide.description}</div>}
+          </div>
           );
         })}
       </section>
--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -3,7 +3,7 @@
 export const SliderData = [
     {
       image:
-        '/images/salibungot_beach.JPG',
+       'salibungot_beach.JPG',
       description: `
       Upon arriving at Jomalig island, I noticed this glistening golden sand from afar.
     
@@ -14,14 +14,14 @@ export const SliderData = [
     },
     {
       image:
-        '/images/borawan_island.JPG',
+      'borawan_island.JPG',
       description: `
      The Island is located in a peaceful community at Barangay Lipata,Padre Burgos, Quezon Province route going to the South,The name Borawan was given by a local resident as it's monicker, as the resort name implies it's a combination of the famous tourist destination Boracay Island, with it's White Sand like Beach and Palawanlike, with it's giant rock formations bound in this unique Resort. `,
      title: `Borawan Island`
     },
     {
       image:
-        '/images/sunshine_farm.JPG',
+      'sunshine_farm.JPG',
       description: `
       Sunshine Farm is a sunflower farm located in Lali, Tiaong Quezon. It is only a few hundred meters away from another famous spot in the province, Villa Escudero. The farm was opened in February 2018 and had 4000 sunflower seeds planted.
     `  ,
@@ -29,14 +29,14 @@ export const SliderData = [
     },
     {
       image:
-        '/images/mount_banahaw.JPG',
+      'mount_banahaw.JPG',
       description: `
       Mount Banahaw is the highest peak in the Calabarzon region of the Philippines. This name also refers to a complex that houses three volcanoes. These three volcanoes are Mt. Banahaw, San Cristobal, and Banahaw de Lucban. Mt. Banahaw is the tallest among the three, standing at about 2158 meters. The Mt. Banahaw complex sits on the boundary of Laguna and Quezon province.  `,
       title: `Mount Banahaw`
     },
     {
       image:
-        '/images/villaEscudero.JPG',
+      'villaEscudero.JPG',
       description: `
       Villa Escudero started as a sugarcane and coconut plantation in Tiaong Quezon. In the 1980s, the owners transformed part of this property ] into a resort. Over the decades, Villa Escudero has become a go-to spot for tourists who would like to understand and experience Filipino culture and heritage.
     `,
@@ -44,7 +44,7 @@ export const SliderData = [
     },
     {
       image:
-        '/images/balabag_falls.JPG',
+        'balabag_falls.JPG',
       description:`
       Balagbag Falls is one of the emerging tourist spots in Quezon province, especially for adventurers and nature lovers. It is a multi-tier waterfall surrounded by lush vegetation.
     It’s also very much accessible, some 5 minutes of walk from the parking area. The first layer has a deep pool, fit for swimming. You can climb on one of the cliffs and jump towards the pool.`,
@@ -52,7 +52,7 @@ export const SliderData = [
     },
     {
       image:
-        '/images/mount_mirador.JPG',
+      'mount_mirador.jpg',
         description: `
 
         Mount Mirador is the highest peak in Quezon National Forest Park. With a height of 1200 feet above sea level, Mt Mirador offers a memorable hiking experience.
@@ -63,7 +63,7 @@ export const SliderData = [
     },
     {
       image:
-        '/images/kanaway_beach.JPG',
+      'kanaway_beach.JPG',
         description:`
         Kanawan Beach also lies within Jomalig Island and is one of the off-beaten tourist spots in Quezon province. Just like Salibungot Beach, it also has golden sands that stretch for as much as 400 meters!
       
@@ -72,7 +72,7 @@ export const SliderData = [
     },
     {
       image:
-        '/images/kwebang_lampas.JPG',
+      'kwebang_lampas.JPG',
         description: `
         Puting Buhangin literally means white sands. Unlike in other famous beaches in Quezon province, the sands here are cleaner and whiter. Located on the coast of Pagbilao Grande Island, Puting Buhangin is accessible from Borawan Beach via a 3-40 minute boat ride.
       
@@ -81,7 +81,7 @@ export const SliderData = [
     },
     {
       image:
-        '/images/dampalitan_island.JPG',
+      'dampalitan_island.JPG',
         description: `
         Although it’s popularly promoted as an island, it is a beach on a small peninsula of mainland Quezon province. It has powdery white sands similar to those found in Boracay Island.`,
         title: `Dampalitan Island`
